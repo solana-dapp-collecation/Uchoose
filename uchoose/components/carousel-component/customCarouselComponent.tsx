dddfbea2bf0b213d6cf7ca083aeff1182a42996a
Chebotov Nikolay <spiritkola@hotmail.com>
2021-09-26 01:42:33 +0300

    chore(carousel): Improve showing collection items

--- uchoose/components/carousel-component/customCarouselComponent.tsx
+++ uchoose/components/carousel-component/customCarouselComponent.tsx
@@ -4,6 +4,8 @@ import WithScrollbar from "./withScrollbar";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import {Card} from "antd";
+import { Rate } from 'antd';
+import { Tag, Divider } from 'antd';
 
 // export function CarouselWithCards(props: React.PropsWithChildren<{}>) {
 //     return (<Section>
@@ -32,6 +34,7 @@ export default function CustomCarouselWithCards(props: React.PropsWithChildren<{
     };
     return (<div style={{width: '1000px'}}>
             {/* https://www.npmjs.com/package/react-multi-carousel */}
+            <Divider orientation="left"><b>Pixel Dodge Collection #1</b></Divider>
             <Carousel
                 swipeable={false}
                 draggable={false}
@@ -61,14 +64,19 @@ export default function CustomCarouselWithCards(props: React.PropsWithChildren<{
                             marginRight: '10px',
                             marginBottom: '30px'
                         }}
-                        cover={<img alt="example" src="/pic_1_ex.jpg"/>}
+                        cover={<img alt="example" src="/Pixel-Doge-3916.png"/>}
                     >
+                        <Meta title="Pixel Doge #3916" description="Generated Dodge 3916" />
+                        <br/>
                         <div>
-                            Author: Noname
-                            <br/>
-                            Collections Params
-                            <br/>
-                            Price: 1$
+                            Author: <a href="https://opensea.io/kementari">kementari</a>
+                            <Divider>Properties</Divider>
+                            <Tag color="green">undershirt</Tag>
+                            <Tag color="brown">hat</Tag>
+                            <Divider>Price</Divider>
+                            Price: 1<img alt="example" src="/eth.svg" style={{marginLeft: '2px', width: '10px'}}/>
+                            <Divider>Rating</Divider>
+                            <Rate allowHalf defaultValue={2.5} /> 2.5
                         </div>
                     </Card>
                     {/*<img*/}
@@ -93,14 +101,19 @@ export default function CustomCarouselWithCards(props: React.PropsWithChildren<{
                             marginRight: '10px',
                             marginBottom: '30px'
                         }}
-                        cover={<img alt="example" src="/pic_2_ex.jpg"/>}
+                        cover={<img alt="example" src="/Pixel-Doge-4954.png"/>}
                     >
+                        <Meta title="Pixel Doge #4954" description="Generated Dodge 4954" />
+                        <br/>
                         <div>
-                            Author: Noname
-                            <br/>
-                            Collections Params
-                            <br/>
-                            Price: 2$
+                            Author: <a href="https://opensea.io/kementari">kementari</a>
+                            <Divider>Properties</Divider>
+                            <Tag color="purple">undershirt</Tag>
+                            <Tag color="green">cap</Tag>
+                            <Divider>Price</Divider>
+                            Price: 2<img alt="example" src="/eth.svg" style={{marginLeft: '2px', width: '10px'}}/>
+                            <Divider>Rating</Divider>
+                            <Rate allowHalf defaultValue={3.5} /> 3.5
                         </div>
                     </Card>
                 </div>
@@ -114,14 +127,19 @@ export default function CustomCarouselWithCards(props: React.PropsWithChildren<{
                             marginRight: '10px',
                             marginBottom: '30px'
                         }}
-                        cover={<img alt="example" src="/pic_ex_3.png"/>}
+                        cover={<img alt="example" src="/Pixel-Doge-1604.png"/>}
                     >
+                        <Meta title="Pixel Doge #1604" description="Generated Dodge 1604" />
+                        <br/>
                         <div>
-                            Author: Noname
-                            <br/>
-                            Collections Params
-                            <br/>
-                            Price: 3$
+                            Author: <a href="https://opensea.io/kementari">kementari</a>
+                            <Divider>Properties</Divider>
+                            <Tag color="red">scarf</Tag>
+                            <Tag color="black">iroquois</Tag>
+                            <Divider>Price</Divider>
+                            Price: 3<img alt="example" src="/eth.svg" style={{marginLeft: '2px', width: '10px'}}/>
+                            <Divider>Rating</Divider>
+                            <Rate allowHalf defaultValue={4} /> 4.0
                         </div>
                     </Card>
                 </div>
@@ -135,16 +153,126 @@ export default function CustomCarouselWithCards(props: React.PropsWithChildren<{
                             marginRight: '10px',
                             marginBottom: '30px'
                         }}
-                        cover={<img alt="example" src="https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"/>}
+                        cover={<img alt="example" src="/Pixel-Doge-715.png"/>}
                     >
+                        <Meta title="Pixel Doge #715" description="Generated Dodge 715" />
+                        <br/>
                         <div>
-                            Author: Noname
-                            <br/>
-                            Collections Params
-                            <br/>
-                            Price: 3$
+                            Author: <a href="https://opensea.io/kementari">kementari</a>
+                            <Divider>Properties</Divider>
+                            <Tag color="blue">undershirt</Tag>
+                            <Tag color="purple">cap</Tag>
+                            <Divider>Price</Divider>
+                            Price: 2<img alt="example" src="/eth.svg" style={{marginLeft: '2px', width: '10px'}}/>
+                            <Divider>Rating</Divider>
+                            <Rate allowHalf defaultValue={3.5} /> 3.5
                         </div>
-                    </Card></div>
+                    </Card>
+                </div>
+                <div>
+                    <Card
+                        hoverable
+                        style={{
+                            width: "90%",
+                            cursor: "pointer",
+                            marginLeft: '10px',
+                            marginRight: '10px',
+                            marginBottom: '30px'
+                        }}
+                        cover={<img alt="example" src="/Pixel-Doge-5481.png"/>}
+                    >
+                        <Meta title="Pixel Doge #5481" description="Generated Dodge 5481" />
+                        <br/>
+                        <div>
+                            Author: <a href="https://opensea.io/kementari">kementari</a>
+                            <Divider>Properties</Divider>
+                            <Tag color="green">undershirt</Tag>
+                            <Tag color="red">horns</Tag>
+                            <Divider>Price</Divider>
+                            Price: 2<img alt="example" src="/eth.svg" style={{marginLeft: '2px', width: '10px'}}/>
+                            <Divider>Rating</Divider>
+                            <Rate allowHalf defaultValue={3} /> 3.0
+                        </div>
+                    </Card>
+                </div>
+                <div>
+                    <Card
+                        hoverable
+                        style={{
+                            width: "90%",
+                            cursor: "pointer",
+                            marginLeft: '10px',
+                            marginRight: '10px',
+                            marginBottom: '30px'
+                        }}
+                        cover={<img alt="example" src="/Pixel-Doge-5154.png"/>}
+                    >
+                        <Meta title="Pixel Doge #5154" description="Generated Dodge 5154" />
+                        <br/>
+                        <div>
+                            Author: <a href="https://opensea.io/kementari">kementari</a>
+                            <Divider>Properties</Divider>
+                            <Tag color="purple">neckerchief</Tag>
+                            <Tag color="brown">arrow</Tag>
+                            <Divider>Price</Divider>
+                            Price: 2<img alt="example" src="/eth.svg" style={{marginLeft: '2px', width: '10px'}}/>
+                            <Divider>Rating</Divider>
+                            <Rate allowHalf defaultValue={2.5} /> 2.5
+                        </div>
+                    </Card>
+                </div>
+                <div>
+                    <Card
+                        hoverable
+                        style={{
+                            width: "90%",
+                            cursor: "pointer",
+                            marginLeft: '10px',
+                            marginRight: '10px',
+                            marginBottom: '30px'
+                        }}
+                        cover={<img alt="example" src="/Pixel-Doge-598.png"/>}
+                    >
+                        <Meta title="Pixel Doge #598" description="Generated Dodge 598" />
+                        <br/>
+                        <div>
+                            Author: <a href="https://opensea.io/kementari">kementari</a>
+                            <Divider>Properties</Divider>
+                            <Tag color="blue">sweater</Tag>
+                            <Tag color="black">iroquois</Tag>
+                            <Divider>Price</Divider>
+                            Price: 2<img alt="example" src="/eth.svg" style={{marginLeft: '2px', width: '10px'}}/>
+                            <Divider>Rating</Divider>
+                            <Rate allowHalf defaultValue={3.2} /> 3.2
+                        </div>
+                    </Card>
+                </div>
+                <div>
+                    <Card
+                        hoverable
+                        style={{
+                            width: "90%",
+                            cursor: "pointer",
+                            marginLeft: '10px',
+                            marginRight: '10px',
+                            marginBottom: '30px'
+                        }}
+                        cover={<img alt="example" src="/Pixel-Doge-5447.png"/>}
+                    >
+                        <Meta title="Pixel Doge #5447" description="Generated Dodge 5447" />
+                        <br/>
+                        <div>
+                            Author: <a href="https://opensea.io/kementari">kementari</a>
+                            <Divider>Properties</Divider>
+                            <Tag color="red">undershirt</Tag>
+                            <Tag color="yellow">cap</Tag>
+                            <Divider>Price</Divider>
+                            Price: 2<img alt="example" src="/eth.svg" style={{marginLeft: '2px', width: '10px'}}/>
+                            <Divider>Rating</Divider>
+                            <Rate allowHalf defaultValue={4.1} /> 4.1
+                        </div>
+                    </Card>
+                </div>
             </Carousel>
         </div>
     )
