f797f37793d56a1d8c48850e69c618a0c4fc9d69
Chebotov Nikolay <spiritkola@hotmail.com>
2021-10-02 14:10:29 +0300

    update(Utils): Add common contracts

---
+++ uchoose-server/src/Uchoose.Utils/Contracts/Common/IMessage.cs
@@ -0,0 +1,28 @@
+﻿// ------------------------------------------------------------------------------------------------------
+// <copyright file="IMessage.cs" company="Life Loop">
+// Copyright (c) Life Loop, 2021. All rights reserved.
+// The core dev team: Nikolay Chebotov (unchase), Leonov Dmitry (gunfighter).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// ------------------------------------------------------------------------------------------------------
+
+using System;
+
+namespace Uchoose.Utils.Contracts.Common
+{
+    /// <summary>
+    /// Сообщение.
+    /// </summary>
+    public interface IMessage
+    {
+        /// <summary>
+        /// Тип сообщения.
+        /// </summary>
+        string MessageType { get; }
+
+        /// <summary>
+        /// Идентификатор агрегата.
+        /// </summary>
+        Guid AggregateId { get; }
+    }
+}
---
+++ uchoose-server/src/Uchoose.Utils/Contracts/Common/IPaginated.cs
@@ -0,0 +1,26 @@
+﻿// ------------------------------------------------------------------------------------------------------
+// <copyright file="IPaginated.cs" company="Life Loop">
+// Copyright (c) Life Loop, 2021. All rights reserved.
+// The core dev team: Nikolay Chebotov (unchase), Leonov Dmitry (gunfighter).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// ------------------------------------------------------------------------------------------------------
+
+namespace Uchoose.Utils.Contracts.Common
+{
+    /// <summary>
+    /// Элемент с пагинацией.
+    /// </summary>
+    public interface IPaginated
+    {
+        /// <summary>
+        /// Текущая страница.
+        /// </summary>
+        public int PageNumber { get; }
+
+        /// <summary>
+        /// Количество единиц данных на страницу.
+        /// </summary>
+        public int PageSize { get; }
+    }
+}
