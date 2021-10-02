01b6079922f0cda666dcfcf3ec7fddf0d9f948af
Chebotov Nikolay <spiritkola@hotmail.com>
2021-10-02 18:05:59 +0300

    update(DataAccess): Add `DataAccess.Interfaces` project
    Add basic context contracts

---
+++ uchoose-server/src/Uchoose.DataAccess.Interfaces/Contexts/ILoggableDbContext.cs
@@ -0,0 +1,25 @@
+﻿// ------------------------------------------------------------------------------------------------------
+// <copyright file="ILoggableDbContext.cs" company="Life Loop">
+// Copyright (c) Life Loop, 2021. All rights reserved.
+// The core dev team: Nikolay Chebotov (unchase), Leonov Dmitry (gunfighter).
+// Licensed under the MIT license. See LICENSE file in the solution root for full license information.
+// </copyright>
+// ------------------------------------------------------------------------------------------------------
+
+using Microsoft.EntityFrameworkCore;
+using Uchoose.Domain.Entities;
+
+namespace Uchoose.DataAccess.Interfaces.Contexts
+{
+    /// <summary>
+    /// Контекст доступа к логируемым данным.
+    /// </summary>
+    public interface ILoggableDbContext :
+        IDbContext
+    {
+        /// <summary>
+        /// Коллекция логов доменных событий.
+        /// </summary>
+        public DbSet<EventLog> EventLogs { get; set; }
+    }
+}
---
+++ uchoose-server/src/Uchoose.DataAccess.Interfaces/Contracts/ISupportsSavingChanges.cs
@@ -0,0 +1,32 @@
+﻿// ------------------------------------------------------------------------------------------------------
+// <copyright file="ISupportsSavingChanges.cs" company="Life Loop">
+// Copyright (c) Life Loop, 2021. All rights reserved.
+// The core dev team: Nikolay Chebotov (unchase), Leonov Dmitry (gunfighter).
+// Licensed under the MIT license. See LICENSE file in the solution root for full license information.
+// </copyright>
+// ------------------------------------------------------------------------------------------------------
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Uchoose.DataAccess.Interfaces.Contracts
+{
+    /// <summary>
+    /// Поддерживает сохранение изменений.
+    /// </summary>
+    public interface ISupportsSavingChanges
+    {
+        /// <summary>
+        /// Сохранить изменения асинхронно.
+        /// </summary>
+        /// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
+        /// <returns>Возвращает количество затронутых записей в базе данных.</returns>
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Сохранить изменения синхронно.
+        /// </summary>
+        /// <returns>Возвращает количество затронутых записей в базе данных.</returns>
+        int SaveChanges();
+    }
+}
