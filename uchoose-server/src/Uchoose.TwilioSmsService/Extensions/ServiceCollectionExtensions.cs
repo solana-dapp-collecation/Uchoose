90db8268e1e7f250fcf9bd23fa6287340c181e29
Chebotov Nikolay <spiritkola@hotmail.com>
2021-10-03 01:06:36 +0300

    update(Services): Implement `TwilioSmsService`

---
+++ uchoose-server/src/Uchoose.TwilioSmsService/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿// ------------------------------------------------------------------------------------------------------
+// <copyright file="ServiceCollectionExtensions.cs" company="Life Loop">
+// Copyright (c) Life Loop, 2021. All rights reserved.
+// The core dev team: Nikolay Chebotov (unchase), Leonov Dmitry (gunfighter).
+// Licensed under the MIT license. See LICENSE file in the solution root for full license information.
+// </copyright>
+// ------------------------------------------------------------------------------------------------------
+
+using Microsoft.Extensions.DependencyInjection;
+using Uchoose.SmsService.Interfaces;
+using Uchoose.Utils.Extensions;
+
+namespace Uchoose.TwilioSmsService.Extensions
+{
+    /// <summary>
+    /// Методы расширения для <see cref="IServiceCollection"/>.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Добавить сервис для работы с СМС.
+        /// </summary>
+        /// <param name="services"><see cref="IServiceCollection"/>.</param>
+        /// <returns>Возвращает <see cref="IServiceCollection"/>.</returns>
+        public static IServiceCollection AddTwilioSmsService(this IServiceCollection services)
+        {
+            return services.
+                AddTransientInfrastructureService<ISmsService, TwilioSmsService>();
+        }
+    }
+}
---
+++ uchoose-server/src/Uchoose.TwilioSmsService/TwilioSmsService.cs
@@ -0,0 +1,52 @@
+﻿// ------------------------------------------------------------------------------------------------------
+// <copyright file="TwilioSmsService.cs" company="Life Loop">
+// Copyright (c) Life Loop, 2021. All rights reserved.
+// The core dev team: Nikolay Chebotov (unchase), Leonov Dmitry (gunfighter).
+// Licensed under the MIT license. See LICENSE file in the solution root for full license information.
+// </copyright>
+// ------------------------------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Options;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Uchoose.SmsService.Interfaces;
+using Uchoose.SmsService.Interfaces.Requests;
+using Uchoose.SmsService.Interfaces.Settings;
+using Uchoose.Utils.Contracts.Services;
+
+namespace Uchoose.TwilioSmsService
+{
+    /// <inheritdoc cref="ISmsService"/>
+    internal sealed class TwilioSmsService : ISmsService, ITransientService
+    {
+        private readonly SmsSettings _settings;
+
+        /// <summary>
+        /// Инициализирует экземпляр <see cref="TwilioSmsService"/>.
+        /// </summary>
+        /// <param name="settings"><see cref="SmsSettings"/>.</param>
+        public TwilioSmsService(
+            IOptionsSnapshot<SmsSettings> settings)
+        {
+            _settings = settings.Value;
+        }
+
+        /// <inheritdoc/>
+        public async Task SendAsync(SmsRequest request)
+        {
+            string accountSid = _settings.SmsAccountIdentification;
+            string authToken = _settings.SmsAccountPassword;
+
+            TwilioClient.Init(accountSid, authToken);
+
+            await MessageResource.CreateAsync(
+                to: new(request.Number),
+                from: new(_settings.SmsAccountFrom),
+                body: request.Message);
+
+            await Task.CompletedTask;
+        }
+    }
+}
