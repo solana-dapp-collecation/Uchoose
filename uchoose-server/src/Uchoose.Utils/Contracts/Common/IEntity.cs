d2f5aba3e9ff54de8606e02d55dfe96c93daf53f
Chebotov Nikolay <spiritkola@hotmail.com>
2021-10-02 14:13:08 +0300

    chore(stylecop.json): Update copyright text

--- uchoose-server/src/Uchoose.Utils/Contracts/Common/IEntity.cs
+++ uchoose-server/src/Uchoose.Utils/Contracts/Common/IEntity.cs
@@ -2,7 +2,7 @@
 // <copyright file="IEntity.cs" company="Life Loop">
 // Copyright (c) Life Loop, 2021. All rights reserved.
 // The core dev team: Nikolay Chebotov (unchase), Leonov Dmitry (gunfighter).
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// Licensed under the MIT license. See LICENSE file in the solution root for full license information.
 // </copyright>
 // ------------------------------------------------------------------------------------------------------
 
