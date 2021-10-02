e160bf93745a07c3d5f27db79532255672cff6fa
Chebotov Nikolay <spiritkola@hotmail.com>
2021-10-02 16:18:28 +0300

    update(Domain): Add `IExtendedAttribute` contract

---
+++ uchoose-server/src/Uchoose.Domain/Enums/ExtendedAttributeType.cs
@@ -0,0 +1,46 @@
+﻿// ------------------------------------------------------------------------------------------------------
+// <copyright file="ExtendedAttributeType.cs" company="Life Loop">
+// Copyright (c) Life Loop, 2021. All rights reserved.
+// The core dev team: Nikolay Chebotov (unchase), Leonov Dmitry (gunfighter).
+// Licensed under the MIT license. See LICENSE file in the solution root for full license information.
+// </copyright>
+// ------------------------------------------------------------------------------------------------------
+
+namespace Uchoose.Domain.Enums
+{
+    /// <summary>
+    /// Тип расширенного атрибута.
+    /// </summary>
+    public enum ExtendedAttributeType : byte
+    {
+        /// <summary>
+        /// Значение с плавающей точкой.
+        /// </summary>
+        Decimal = 1,
+
+        /// <summary>
+        /// Текстовое значение.
+        /// </summary>
+        Text = 2,
+
+        /// <summary>
+        /// Значение даты и времени.
+        /// </summary>
+        DateTime = 3,
+
+        /// <summary>
+        /// Значение в виде json строки.
+        /// </summary>
+        Json = 4,
+
+        /// <summary>
+        /// Логическое значение.
+        /// </summary>
+        Boolean = 5,
+
+        /// <summary>
+        /// Целочисленное значение.
+        /// </summary>
+        Integer = 6
+    }
+}
