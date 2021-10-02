8caf2dd518e2724e60e31c55084cc4502bb721f9
Chebotov Nikolay <spiritkola@hotmail.com>
2021-10-02 17:47:58 +0300

    update(Domain.Identity): Add `UchooseRole` entity

---
+++ uchoose-server/src/Uchoose.Domain.Identity/Entities/UchooseRoleClaim.cs
@@ -0,0 +1,104 @@
+﻿// ------------------------------------------------------------------------------------------------------
+// <copyright file="UchooseRoleClaim.cs" company="Life Loop">
+// Copyright (c) Life Loop, 2021. All rights reserved.
+// The core dev team: Nikolay Chebotov (unchase), Leonov Dmitry (gunfighter).
+// Licensed under the MIT license. See LICENSE file in the solution root for full license information.
+// </copyright>
+// ------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Identity;
+using Uchoose.Domain.Contracts;
+using Uchoose.Utils.Attributes.Exporting;
+using Uchoose.Utils.Attributes.Importing;
+using Uchoose.Utils.Attributes.Searching;
+using Uchoose.Utils.Contracts.Properties;
+
+namespace Uchoose.Domain.Identity.Entities
+{
+    /// <summary>
+    /// Разрешение роли пользователя.
+    /// </summary>
+    public class UchooseRoleClaim :
+        IdentityRoleClaim<Guid>,
+        IAuditableEntity<int>,
+        IDomainEntity,
+        IHasDescription
+    {
+        /// <summary>
+        /// Инициализирует новый экземпляр <see cref="UchooseRoleClaim"/>.
+        /// </summary>
+        /// <remarks>
+        /// Свойство Id инициализируется новым значением GUID.
+        /// </remarks>
+        public UchooseRoleClaim()
+        {
+        }
+
+        /// <summary>
+        /// Инициализирует новый экземпляр <see cref="UchooseRoleClaim"/>.
+        /// </summary>
+        /// <remarks>
+        /// Свойство Id инициализируется новым значением GUID.
+        /// </remarks>
+        /// <param name="roleClaimDescription">Описание.</param>
+        /// <param name="roleClaimGroup">Группа.</param>
+        public UchooseRoleClaim(string roleClaimDescription = null, string roleClaimGroup = null)
+        {
+            Description = roleClaimDescription;
+            Group = roleClaimGroup;
+        }
+
+        /// <inheritdoc/>
+        public Guid CreatedBy { get; set; }
+
+        /// <inheritdoc/>
+        public DateTime CreatedOn { get; set; }
+
+        /// <inheritdoc/>
+        public Guid LastModifiedBy { get; set; }
+
+        /// <inheritdoc/>
+        public DateTime? LastModifiedOn { get; set; }
+
+        /// <summary>
+        /// Описание разрешения.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Группа разрешений.
+        /// </summary>
+        public string Group { get; set; }
+
+        /// <summary>
+        /// Роль пользователя, которой принадлежит разрешение.
+        /// </summary>
+        public virtual UchooseRole Role { get; set; }
+
+#pragma warning disable SA1201 // Elements should appear in the correct order
+        private List<IDomainEvent> _domainEvents;
+#pragma warning restore SA1201 // Elements should appear in the correct order
+
+        /// <inheritdoc/>
+        [NotExportable]
+        [NotImportable]
+        [NotSearchable]
+        public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents?.AsReadOnly();
+
+        /// <inheritdoc/>
+        public void AddDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents ??= new();
+            _domainEvents.Add(domainEvent);
+        }
+
+        /// <inheritdoc/>
+        public void RemoveDomainEvent(IDomainEvent domainEvent) => _domainEvents?.Remove(domainEvent);
+
+        /// <inheritdoc/>
+        public void ClearDomainEvents() => _domainEvents?.Clear();
+    }
+}
---
+++ uchoose-server/src/Uchoose.Utils/Contracts/Properties/IHasName.cs
@@ -0,0 +1,22 @@
+﻿// ------------------------------------------------------------------------------------------------------
+// <copyright file="IHasName.cs" company="Life Loop">
+// Copyright (c) Life Loop, 2021. All rights reserved.
+// The core dev team: Nikolay Chebotov (unchase), Leonov Dmitry (gunfighter).
+// Licensed under the MIT license. See LICENSE file in the solution root for full license information.
+// </copyright>
+// ------------------------------------------------------------------------------------------------------
+
+namespace Uchoose.Utils.Contracts.Properties
+{
+    /// <summary>
+    /// Имеет свойство с именем Name.
+    /// </summary>
+    public interface IHasName :
+        IHasProperty
+    {
+        /// <summary>
+        /// Наименование (имя).
+        /// </summary>
+        string Name { get; }
+    }
+}
