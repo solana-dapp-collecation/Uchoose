6bdc7f751316722ea0c87b228b23a716a2744a31
Chebotov Nikolay <spiritkola@hotmail.com>
2021-10-02 18:32:03 +0300

    update(Services): Add `MailService.Interfaces` project

---
+++ uchoose-server/src/Uchoose.MailService.Interfaces/Requests/MailRequest.cs
@@ -0,0 +1,46 @@
+﻿// ------------------------------------------------------------------------------------------------------
+// <copyright file="MailRequest.cs" company="Life Loop">
+// Copyright (c) Life Loop, 2021. All rights reserved.
+// The core dev team: Nikolay Chebotov (unchase), Leonov Dmitry (gunfighter).
+// Licensed under the MIT license. See LICENSE file in the solution root for full license information.
+// </copyright>
+// ------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Http;
+
+namespace Uchoose.MailService.Interfaces.Requests
+{
+    /// <summary>
+    /// Запрос на отправку email сообщения.
+    /// </summary>
+    public class MailRequest
+    {
+        /// <summary>
+        /// Адреса получателей.
+        /// </summary>
+        public string[] To { get; set; } = Array.Empty<string>();
+
+        /// <summary>
+        /// Тема письма.
+        /// </summary>
+        public string Subject { get; set; }
+
+        /// <summary>
+        /// Тело письма.
+        /// </summary>
+        public string Body { get; set; }
+
+        /// <summary>
+        /// Адрес отправителя.
+        /// </summary>
+        public string From { get; set; }
+
+        /// <summary>
+        /// Список прикреплённых файлов.
+        /// </summary>
+        public List<IFormFile> Attachments { get; set; } = new();
+    }
+}
---
+++ uchoose-server/src/Uchoose.MailService.Interfaces/Requests/MailTemplateRequest.cs
@@ -0,0 +1,47 @@
+﻿// ------------------------------------------------------------------------------------------------------
+// <copyright file="MailTemplateRequest.cs" company="Life Loop">
+// Copyright (c) Life Loop, 2021. All rights reserved.
+// The core dev team: Nikolay Chebotov (unchase), Leonov Dmitry (gunfighter).
+// Licensed under the MIT license. See LICENSE file in the solution root for full license information.
+// </copyright>
+// ------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+
+namespace Uchoose.MailService.Interfaces.Requests
+{
+    /// <summary>
+    /// Запрос на отправку email сообщения из шаблона.
+    /// </summary>
+    public class MailTemplateRequest
+    {
+        /// <summary>
+        /// Адреса получателей.
+        /// </summary>
+        public string[] To { get; set; } = Array.Empty<string>();
+
+        /// <summary>
+        /// Тема письма.
+        /// </summary>
+        public string Subject { get; set; }
+
+        /// <summary>
+        /// Адрес отправителя.
+        /// </summary>
+        public string From { get; set; }
+
+        /// <summary>
+        /// Имя используемого шаблона.
+        /// </summary>
+        public string TemplateName { get; set; }
+
+        /// <summary>
+        /// Подстановки (замены) в тексте шаблона.
+        /// </summary>
+        /// <remarks>
+        /// Ключ - заменяемый текст, значение - на что заменить.
+        /// </remarks>
+        public Dictionary<string, string> Substitutions { get; set; } = new();
+    }
+}
