310ecac83996f588e05e5ca7afc66c7cd836383e
Chebotov Nikolay <spiritkola@hotmail.com>
2021-10-03 11:09:39 +0300

    update(Services): Implement `DateTimeService`

---
+++ uchoose-server/src/Uchoose.DateTimeService/DateTimeService.cs
@@ -0,0 +1,33 @@
+﻿// ------------------------------------------------------------------------------------------------------
+// <copyright file="DateTimeService.cs" company="Life Loop">
+// Copyright (c) Life Loop, 2021. All rights reserved.
+// The core dev team: Nikolay Chebotov (unchase), Leonov Dmitry (gunfighter).
+// Licensed under the MIT license. See LICENSE file in the solution root for full license information.
+// </copyright>
+// ------------------------------------------------------------------------------------------------------
+
+using System;
+
+using Uchoose.DateTimeService.Interfaces;
+using Uchoose.Utils.Contracts.Services;
+
+namespace Uchoose.DateTimeService
+{
+    /// <inheritdoc cref="IDateTimeService"/>
+    internal sealed class DateTimeService :
+        IDateTimeService,
+        IScopedService
+    {
+        /// <inheritdoc/>
+        public DateTime NowUtc => DateTime.UtcNow;
+
+        /// <inheritdoc/>
+        public DateTimeOffset NowUtcOffset => DateTimeOffset.UtcNow;
+
+        /// <inheritdoc/>
+        public long NowUtcTimestampMilliseconds => DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+
+        /// <inheritdoc/>
+        public DateTime NowLocalTime => DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Local);
+    }
+}
