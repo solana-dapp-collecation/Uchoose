630689e8dd11203fb0175d8dd1b93dc465c3bb7d
Chebotov Nikolay <spiritkola@hotmail.com>
2021-10-03 11:29:26 +0300

    update(Services): Implement `SerializationService`

---
+++ uchoose-server/src/Uchoose.SerializationService/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,44 @@
+﻿// ------------------------------------------------------------------------------------------------------
+// <copyright file="ServiceCollectionExtensions.cs" company="Life Loop">
+// Copyright (c) Life Loop, 2021. All rights reserved.
+// The core dev team: Nikolay Chebotov (unchase), Leonov Dmitry (gunfighter).
+// Licensed under the MIT license. See LICENSE file in the solution root for full license information.
+// </copyright>
+// ------------------------------------------------------------------------------------------------------
+
+using System.Linq;
+using System.Text.Json;
+
+using Microsoft.Extensions.DependencyInjection;
+using Uchoose.SerializationService.Interfaces;
+using Uchoose.SerializationService.Interfaces.Converters;
+using Uchoose.SerializationService.Serializers;
+
+namespace Uchoose.SerializationService.Extensions
+{
+    /// <summary>
+    /// Методы расширения для <see cref="IServiceCollection"/>.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Добавить сериализатор json.
+        /// </summary>
+        /// <param name="services"><see cref="IServiceCollection"/>.</param>
+        /// <returns>Возвращает <see cref="IServiceCollection"/>.</returns>
+        public static IServiceCollection AddJsonSerializer(this IServiceCollection services)
+        {
+            services
+                .AddSingleton<IJsonSerializer, SystemTextJsonSerializer>()
+                .Configure<JsonSerializerOptions>(configureOptions =>
+                {
+                    if (!configureOptions.Converters.Any(c => c.GetType() == typeof(TimespanJsonConverter)))
+                    {
+                        configureOptions.Converters.Add(new TimespanJsonConverter());
+                    }
+                });
+
+            return services;
+        }
+    }
+}
---
+++ uchoose-server/src/Uchoose.SerializationService/Serializers/SystemTextJsonSerializer.cs
@@ -0,0 +1,54 @@
+﻿// ------------------------------------------------------------------------------------------------------
+// <copyright file="SystemTextJsonSerializer.cs" company="Life Loop">
+// Copyright (c) Life Loop, 2021. All rights reserved.
+// The core dev team: Nikolay Chebotov (unchase), Leonov Dmitry (gunfighter).
+// Licensed under the MIT license. See LICENSE file in the solution root for full license information.
+// </copyright>
+// ------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Text.Json;
+
+using Microsoft.Extensions.Options;
+using Uchoose.SerializationService.Interfaces;
+using Uchoose.SerializationService.Interfaces.Converters;
+
+namespace Uchoose.SerializationService.Serializers
+{
+    /// <summary>
+    /// Сериализатор json с помощью <see cref="System.Text.Json"/>.
+    /// </summary>
+    public class SystemTextJsonSerializer :
+        IJsonSerializer
+    {
+        private readonly JsonSerializerOptions _options;
+
+        /// <summary>
+        /// Инициализирует экземпляр <see cref="SystemTextJsonSerializer"/>.
+        /// </summary>
+        /// <param name="options"><see cref="JsonSerializerOptions"/>.</param>
+        public SystemTextJsonSerializer(IOptions<JsonSerializerOptions> options)
+        {
+            _options = options.Value;
+
+            // TODO - или конфигурировать не здесь?
+            if (!_options.Converters.Any(c => c.GetType() == typeof(TimespanJsonConverter)))
+            {
+                _options.Converters.Add(new TimespanJsonConverter());
+            }
+        }
+
+        /// <inheritdoc/>
+        public T Deserialize<T>(string data, JsonSerializerOptions options = null)
+            => JsonSerializer.Deserialize<T>(data, options ?? _options);
+
+        /// <inheritdoc/>
+        public string Serialize<T>(T data, JsonSerializerOptions options = null)
+            => JsonSerializer.Serialize(data, options ?? _options);
+
+        /// <inheritdoc/>
+        public string Serialize<T>(T data, Type type, JsonSerializerOptions options = null)
+            => JsonSerializer.Serialize(data, type, options ?? _options);
+    }
+}
