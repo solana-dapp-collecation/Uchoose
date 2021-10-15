6066b66ada66c414324c8b2a4b2bf9d8bbd2766d
Chebotov Nikolay <spiritkola@hotmail.com>
2021-10-15 23:44:16 +0300

    chore(stylecop.json): Update license (replace MIT to Commercial)

--- uchoose-server/src/Uchoose.Domain/Contracts/IDomainEntity.cs
+++ uchoose-server/src/Uchoose.Domain/Contracts/IDomainEntity.cs
@@ -2,7 +2,7 @@
 // <copyright file="IDomainEntity.cs" company="Life Loop">
 // Copyright (c) Life Loop, 2021. All rights reserved.
 // The core dev team: Nikolay Chebotov (unchase), Leonov Dmitry (gunfighter).
-// Licensed under the MIT license. See LICENSE file in the solution root for full license information.
+// The Application under the Commercial license. See LICENSE file in the solution root for full license information.
 // </copyright>
 // ------------------------------------------------------------------------------------------------------
 
